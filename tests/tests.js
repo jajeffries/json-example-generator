d31eae60428e8ba60acda85938f02e5684be028c
developer <James Jeffries>
2015-08-04 13:53:59 +0100

    refactoring

--- tests/tests.js
+++ tests/tests.js
@@ -12,7 +12,7 @@ describe('Generator', function () {
 				"type": "object",
 				"properties": {
 					"name": {
-						"description": "Name of the product",
+						"description": "Name of the example",
 						"type": "string"
 					}
 				}
@@ -27,5 +27,29 @@ describe('Generator', function () {
 				assert.ok(_.isEqual(expected, example));
 			});
 		});
+
+		// describe('from schema with number attribute type', function () {
+		// 	var exampleSchema = {
+		// 		"$schema": "http://json-schema.org/draft-04/schema#",
+		// 		"title": "Product",
+		// 		"description": "Test data with number",
+		// 		"type": "object",
+		// 		"properties": {
+		// 			"id": {
+		// 				"description": "ID of the example",
+		// 				"type": "number"
+		// 			}
+		// 		}
+		// 	};
+		// 	it('should generate example with number', function () {
+		// 		console.log(Generator);
+
+		// 		var example = new Generator(exampleSchema).generate(),
+		// 			expected = {
+		// 				id: 0
+		// 			};
+		// 		assert.ok(_.isEqual(expected, example));
+		// 	});
+		// });
 	});
 });
